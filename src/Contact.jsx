b22e0e136bef6bdea42f4ade07f64bb5c1544102
nbr41to <nbr.41to@gmail.com>
2020-10-08 21:54:37 +0900

    .

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -2,9 +2,6 @@ import React from 'react'
 import styled from 'styled-components'
 // images
 import top_view from "./images/undraw_team_spirit_hrr4.png"
-import section_view1 from "./images/orange-one-human.png"
-import section_view2 from "./images/red-one-human.png"
-import section_view3 from "./images/three-humans.png"
 // components
 import Section from "./components/Section"
 import TopView from "./components/TopView"
@@ -23,49 +20,7 @@ function Contact() {
 }
 
 const StyledComponent = styled.div`
-    .top-view {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        .top-view-text {
-                display: flex;
-                flex-direction: column;
-                justify-content: center;
-                align-items: center;
-                font-family : 'Arial' ,sans-serif;
-            h2 {
-                margin: 10px;
-                &:before, &:after {
-                    content: '-';
-                    margin: 0 8px;
-                }
-            }
-        }
-        img {
-            width: 50%;
-        }
-    }
-    section {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        padding: 30px;
-        img {
-            max-width: 400px;
-            max-height: 400px;
-            margin: 12px;
-        }
-        .text-box {
-            padding: 12px;
-            margin:12px;
-            h3 {
-                margin: 8px 0;
-            }
-            p {
-
-            }
-        }
-    }
+    
 `
 
 export default Contact
