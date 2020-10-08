f160336419b1286ad444f65685bde5fc5a0f9526
nbr41to <nbr.41to@gmail.com>
2020-10-08 13:56:50 +0900

    Updates

---
+++ precache-manifest.88a2ae92424ff3769f414a853754a82e.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5df6c85e3323a007e2ff65e7a4af7687",
+    "url": "/yasumelt-lp/index.html"
+  },
+  {
+    "revision": "2efe47bb99db9e5b5211",
+    "url": "/yasumelt-lp/static/js/2.11b63a0e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/yasumelt-lp/static/js/2.11b63a0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c56c566a10a263895634",
+    "url": "/yasumelt-lp/static/js/main.a56f6d9c.chunk.js"
+  },
+  {
+    "revision": "7ac66ea948be09a3932f",
+    "url": "/yasumelt-lp/static/js/runtime-main.f9974d26.js"
+  },
+  {
+    "revision": "48d858df1cc5cb9fbe2370995f95c146",
+    "url": "/yasumelt-lp/static/media/clip-reading-of-books-1.48d858df.png"
+  },
+  {
+    "revision": "63ce9e8e7d27568cd249e66013f08217",
+    "url": "/yasumelt-lp/static/media/clip-remote-work-1.63ce9e8e.png"
+  },
+  {
+    "revision": "6eb52e6b7bed6ce10f7711511e2ae5ac",
+    "url": "/yasumelt-lp/static/media/developer-team.6eb52e6b.png"
+  },
+  {
+    "revision": "34be340301938764c0aed29bfc32a725",
+    "url": "/yasumelt-lp/static/media/flame-1049.34be3403.png"
+  },
+  {
+    "revision": "813e6a26318ace2ccf8ce57f264aa7cc",
+    "url": "/yasumelt-lp/static/media/gummy-programming.813e6a26.png"
+  },
+  {
+    "revision": "6bdb62b4f62a32706420358df554207f",
+    "url": "/yasumelt-lp/static/media/orange-one-human.6bdb62b4.png"
+  },
+  {
+    "revision": "19d5a08edfd7f65029aafc8c94ac8a6f",
+    "url": "/yasumelt-lp/static/media/red-one-human.19d5a08e.png"
+  },
+  {
+    "revision": "2e90b333f4fc18d4c221cad16f7e4842",
+    "url": "/yasumelt-lp/static/media/three-humans.2e90b333.png"
+  },
+  {
+    "revision": "775814e09760920b3a20e15fc691d5f8",
+    "url": "/yasumelt-lp/static/media/undraw_pair_programming_njlp.775814e0.png"
+  },
+  {
+    "revision": "d8b7db808eafe0584f188b3d3e79a776",
+    "url": "/yasumelt-lp/static/media/undraw_react_y7wq.d8b7db80.png"
+  },
+  {
+    "revision": "bb50d0631bc12d53ab004cd36d0fb1f8",
+    "url": "/yasumelt-lp/static/media/undraw_team_spirit_hrr4.bb50d063.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.982e2035571387444a0fd7383de9e6d7.js"
+  "/yasumelt-lp/precache-manifest.88a2ae92424ff3769f414a853754a82e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/yasumelt-lp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
